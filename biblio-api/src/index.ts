11752277ed3b69d3e55c724f3bbf45f718dc291e
Émile <69686396+Kkiriya@users.noreply.github.com>
2026-06-09 10:32:37 -0400

    finished writing index.ts

---
+++ biblio-api/src/index.ts
@@ -0,0 +1,49 @@
+import prisma from "../utils/prisma";
+
+async function seed() {
+  const livres = await prisma.livre.createMany({
+    data: [
+      {
+        titre: "Le Petit Prince",
+        auteur: "Antoine de Saint-Exupéry",
+        annee: 1943,
+        disponible: true,
+      },
+      {
+        titre: "1984",
+        auteur: "George Orwell",
+        annee: 1949,
+        disponible: true,
+      },
+      {
+        titre: "Supermarket",
+        auteur: "Bobby Hall",
+        annee: 2019,
+        disponible: false,
+      },
+      {
+        titre: "The Midnight Library",
+        auteur: "Matt Haig",
+        annee: 2020,
+        disponible: true,
+      },
+      {
+        titre: "Royal",
+        auteur: "Jean-Philippe Baril Guérard",
+        annee: 2016,
+        disponible: false,
+      },
+    ],
+  });
+  console.log("Créé :", livres);
+}
+
+async function main() {
+  await seed();
+  await prisma.$disconnect();
+}
+
+main.catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
